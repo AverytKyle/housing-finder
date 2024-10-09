0c9402b60cd1768513d7f1758772787c20ab567b
kyle <averytkyle@gmail.com>
2024-10-09 18:21:12 -0400

    login

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
 // Sign up
@@ -24,5 +27,49 @@ router.post('/', async (req, res) => {
         user: safeUser
     });
 });
+
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .notEmpty()
+        .withMessage('Email is required.')
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('User name is required.')
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Password is required.')
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+router.post('/', validateSignup, async (req, res) => {
+        const { email, password, username } = req.body;
+        const hashedPassword = bcrypt.hashSync(password);
+        const user = await User.create({ email, username, hashedPassword });
+
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        };
+
+        await setTokenCookie(res, safeUser);
+
+        return res.json({
+            user: safeUser
+        });
+    });
 
 module.exports = router;
