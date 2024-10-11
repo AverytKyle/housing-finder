e48efb2f42d6c2d9d31b4d0e17cb2a46e9493cf5
kyle <averytkyle@gmail.com>
2024-10-10 20:54:11 -0400

    changes

--- backend/db/migrations/20241011003902-add-first-last-name-to-users.js
+++ backend/db/migrations/20241011003902-add-first-last-name-to-users.js
@@ -5,21 +5,22 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
+options.tableName = "Users";
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn(options, 'firstName', {
       type: Sequelize.STRING,
       allowNull: true
     }, options);
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn(options, 'lastName', {
       type: Sequelize.STRING,
       allowNull: true
-    }, options);
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = "Users";
+  async down(queryInterface, Sequelize) {
     return queryInterface.dropTable(options);
   }
 };
