4281d9bbc899ff3a25f28b98f2c408b9f7171c81
kyle <averytkyle@gmail.com>
2024-11-28 11:31:52 -0500

    Added Navigation

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import { AiOutlineUser } from "react-icons/ai";
+
+const ProfileButton = () => {
+    return (
+        <div style={{ fontSize: '25px' }}>
+            <AiOutlineUser />
+        </div>
+    )
+};
+
+export default ProfileButton;
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -46,6 +46,15 @@ export const signup = (user) => async dispatch => {
     return response;
 };
 
+export const logout = () => async dispatch => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE'
+    });
+    const data = await response.json();
+    dispatch(removeUser());
+    return response;
+}
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
