09ed2f5e90951eab4b21943e3e26e63b88f1bdef
kyle <averytkyle@gmail.com>
2024-11-28 12:08:13 -0500

    user profile drop down menu

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,11 +1,54 @@
 import { AiOutlineUser } from "react-icons/ai";
+import { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from '../../store/session';
 
-const ProfileButton = () => {
+const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
     return (
-        <div style={{ fontSize: '25px' }}>
-            <AiOutlineUser />
-        </div>
-    )
+        <>
+            <button onClick={toggleMenu}>
+                <AiOutlineUser style={{ fontSize: '15px' }} />
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </>
+    );
 };
 
 export default ProfileButton;
