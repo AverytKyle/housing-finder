8aeea9d0faeb68e62fd879a52e4646e084bfb130
kyle <averytkyle@gmail.com>
2024-12-03 21:06:16 -0500

    spot details updated

--- frontend/src/components/Spots/SpotDetails.jsx
+++ frontend/src/components/Spots/SpotDetails.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSpotById } from '../../store/spots';
-import './Spots.css';
+import './SpotDetails.css';
 
 function SpotDetails() {
     const { spotId } = useParams();
@@ -13,11 +13,33 @@ function SpotDetails() {
         dispatch(getSpotById(spotId));
     }, [dispatch, spotId]);
 
+    if (!spotDetails || !spotDetails.SpotImages) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className='spot-details'>
-            <h2>{spotDetails.name}</h2>
-            <h3>{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</h3>
-        </div>
+            <h2 className='spot-name'>{spotDetails.name}</h2>
+            <h3 className='location'>{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</h3>
+            {spotDetails.SpotImages.map(image => (
+                <div key={spotDetails.id} className='img-container'>
+                    <img src={image.url} />
+                </div>
+            ))}
+            <div className='descrip-price'>
+                <div>
+                    <h2 className='owner-name'>Hosted by {spotDetails.Owner.firstName} {spotDetails.Owner.lastName}</h2>
+                    <p className='description'>{spotDetails.description}</p>
+                </div>
+                <div key={spotDetails.id} className='price-reserve-box'>
+                    <p className='price'>${spotDetails.price} per night</p>
+                    <p className='raiting-reviews'>{spotDetails.avgStarRating} Stars - Reviews: {spotDetails.numReviews}</p>
+                    <div className='button'>
+                        <button className='reserve-button' type='submit'>Reserve</button>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
 
